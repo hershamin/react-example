149a91f72a347d074424340b50a6de7ac0d601e0
Hersh Amin <h.amin@utexas.edu>
2021-09-26 17:45:48 -0500

    Added state management and router (#2)
    * Added state management and router
    
    * Fixed linting
    
    * Fixed tests

--- src/app/App.js
+++ src/app/App.js
@@ -1,23 +1,39 @@
+import { Switch, Route } from 'react-router-dom';
+import routes from '../routes';
 import logo from '../logo.svg';
+
+import { ExampleStore } from '../stores/example';
+
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App = () => {
+  const clickCount = ExampleStore.useState(s => s.clickCount);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>{clickCount} click{clickCount === 1 ? '' : 's'}</p>
+        <a
+          className="App-link"
+          href="#/"
+          rel="noopener noreferrer"
+          onClick={() => ExampleStore.update(s => {
+            s.clickCount = clickCount + 1
+          })}
+        >
+          Click
+        </a>
+        <Switch>
+          <Route path={routes.r1} component={() => (<span>R1 route</span>)} />
+          <Route path={routes.r2} component={() => (<span>R2 route</span>)} />
+        </Switch>
+      </header>
+    </div>
+  );
+};
 
 export default App;
